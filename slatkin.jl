c0d5ea4391d46df7b74e1aca731cc059de2fb1f0
George Lesica <george@lesica.com>
2016-05-06 00:13:28 -0600

    Switch to Int32 and add second example.

--- slatkin.jl
+++ slatkin.jl
@@ -1,24 +1,29 @@
 # Julia bindings for slatkin-exact-tools
+# For now, be sure to run `make shared` before using.
 
 immutable SlatkinResult
   probability::Cdouble
   theta_estimate::Cdouble
 end
 
-function montecarlo(num_reps::Int, obs_list::Vector{Int},
-    numalleles::Int, parallel::Int)
-  obs = [0; obs_list; 0]
+function montecarlo(num_reps::Int32, obs_list::Vector{Int32},
+    numalleles::Int32, parallel::Int32)
+  obs = Int32[0; obs_list; 0]
 
   ccall((:slatkin_mc, "./slatkin.so"), SlatkinResult,
-      (Int, Ptr{Int}), num_reps, obs)
+      (Int, Ptr{Int32}), num_reps, obs)
 end
 
-function ewens_montecarlo(num_reps::Int, obs_list::Vector{Int})
-  parallel = 0
-  padded_numalleles = length(obs_list) + 2
+function ewens_montecarlo(num_reps::Int32, obs_list::Vector{Int32})
+  parallel::Int32 = 0
+  padded_numalleles::Int32 = length(obs_list) + 2
 
   montecarlo(num_reps, obs_list, padded_numalleles, parallel)
 end
 
-println(ewens_montecarlo(100000, [8, 4, 3, 2, 2, 1, 1, 1, 1, 1, 1]))
+counts1 = Int32[8, 4, 3, 2, 2, 1, 1, 1, 1, 1, 1]
+ewens_montecarlo(Int32(100000), counts1) |> println
+
+counts2 = Int32[91, 56, 27, 9, 7, 3, 1]
+ewens_montecarlo(Int32(100000), counts2) |> println
 
